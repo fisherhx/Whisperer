e08b859c772d530315fcdfa88bfb3aaba654d3c4
fisherhx <e0003437@u.nus.edu>
2018-04-20 09:02:09 +0800

    Add in files

---
+++ app/src/main/java/com/example/fishe/KhooKayLengDesmond_HeeHanXiang_CS3218/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val RECORD_AUDIO_REQUEST_CODE = 101
+
+    lateinit private var soundTransmitter: SoundTransmitter
+
+    companion object {
+        lateinit var buffer: ShortArray
+        var soundFFTMag: DoubleArray  = DoubleArray(1024)
+
+        var FFT_Len = 512
+        var bufferSize: Int = 0     // in bytes, will be altered in SoundSampler.kt
+
+        val FS = 44100     // sampling frequency
+        var mx = -99999.0
+        var freqResolution: Double = FS * 1.0 / FFT_Len
+        val offset = 10
+        val startPw = 201
+        val endPw = 210
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // seek permission to do audio recording
+        setupPermissions()
+        initiateSoundTransmitting()
+
+        sendButton?.setOnClickListener {
+            val msgToSend: String? = textMessage?.text.toString()
+            val charset = Charsets.UTF_8
+            var sizeMsg:Int = msgToSend!!.length
+            var tempString = ""
+            var currLetter = ""
+            var prevLetter = ""
+            var isRepeated = false
+            var count = 0
+            if (msgToSend.isNullOrBlank()) {
+                textMessage?.error = "Empty Message"
+                textMessage?.requestFocus()
+                return@setOnClickListener
+            }
+            textMessage!!.text.clear()
+
+            for(index in msgToSend.indices){
+                currLetter = msgToSend[index].toString()
+                //Convert Capital A,B,C to ascii values 123-125
+                if(currLetter.equals("A")){
+                    currLetter = "{"
+                }
+                if(currLetter.equals("B")){
+                    currLetter = "|"
+                }
+                if(currLetter.equals("C")){
+                    currLetter = "}"
+                }
+                //If the letter is repeated, start counting number of time it is repeated
+                if(currLetter.equals(prevLetter)){
+                    count++
+                    isRepeated = true
+                    // If the curr letter is the last char in the string, append it along with the count
+                    if(index == sizeMsg-1){
+                        var countString = (count).toString()
+                        tempString = tempString + '/' + countString + '.' + prevLetter
+                    }
+                }
+                //If the letter is not repeated, append the letter to the msg depending
+                else{
+                    //Non repeated, simply append
+                    if(!isRepeated){
+                        tempString = tempString + currLetter
+                        prevLetter = currLetter
+                    }
+                    //Repeated Char, append with special char to indicate start and end of repeating
+                    else{
+                        var countString = count.toString()
+                        tempString = tempString + '/' + countString + '.' + prevLetter + currLetter
+                        prevLetter = currLetter
+                        isRepeated = false
+                        count = 0
+                    }
+                }
+            }
+
+            originalText.text = msgToSend
+
+            val byteArray = tempString?.toByteArray(charset)
+
+            Toast.makeText(this, byteArray?.contentToString() , Toast.LENGTH_LONG)
+                    .show()
+            transformedText.text = byteArray?.toString(charset)
+            transmitMessage(byteArray)
+        }
+
+        val button1 = findViewById<Button>(R.id.soundReceive)
+        button1.setOnClickListener {
+            val intent = Intent(this, SoundReceiver::class.java).apply {
+            }
+            startActivity(intent)
+        }
+    }
+
+    private fun setupPermissions() {
+        val permission = checkSelfPermission(Manifest.permission.RECORD_AUDIO)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("tag", "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO),
+                RECORD_AUDIO_REQUEST_CODE)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            RECORD_AUDIO_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("tag", "Permission has been denied by user")
+                } else {
+                    Log.i("tag", "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    private fun initiateSoundTransmitting() {
+        try {
+            soundTransmitter = SoundTransmitter(this)
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, "Cannot instantiate SoundTransmitter", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun transmitMessage(byteArray: ByteArray?) {
+        var size:Int = byteArray!!.size
+        soundTransmitter.playSound(freqResolution * startPw, 0.2)
+        for(i in 0..size-1) {
+            soundTransmitter.playSound(freqResolution * 205, 0.2)
+            soundTransmitter.playSound(freqResolution * (offset + 2 * (byteArray?.get(i)!!.toInt() - 32)), 0.3)
+        }
+        soundTransmitter.playSound(freqResolution * endPw, 0.2)
+    }
+
+}
---
+++ app/src/main/java/com/example/fishe/KhooKayLengDesmond_HeeHanXiang_CS3218/SoundSampler.kt
@@ -0,0 +1,225 @@
+package com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218
+
+import android.graphics.Bitmap
+import android.media.AudioRecord
+import android.util.Log
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.FFT_Len
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.FS
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.bufferSize
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.buffer
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.soundFFTMag
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.mx
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.offset
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.startPw
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.MainActivity.Companion.endPw
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.msg
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.prevIndex
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.currCount
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.isRepeated
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.isCounted
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.count
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.isEnd
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.isStarted
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.repeatedWord
+import com.example.fishe.KhooKayLengDesmond_HeeHanXiang_CS3218.SoundReceiver.Companion.singalRecieved
+
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D
+
+class SoundSampler(activity: SoundReceiver) {
+    var audioRecord: AudioRecord? = null
+    private val audioEncoding = 2
+    private val nChannels = 16
+    var isRecording = false
+
+    private var recordingThread: Thread? = null
+
+
+    @Throws(Exception::class)
+    fun init() {
+
+        bufferSize = AudioRecord.getMinBufferSize(FS, nChannels, audioEncoding)
+        buffer = ShortArray(bufferSize)
+        isRecording = true
+
+        try {
+            if (audioRecord != null) {
+                audioRecord!!.stop()
+                audioRecord!!.release()
+            }
+            audioRecord = AudioRecord(1, FS, nChannels, audioEncoding, bufferSize)
+
+        } catch (e: Exception) {
+            Log.d("Error in Init() ", e.message)
+            throw Exception()
+        }
+
+        audioRecord!!.startRecording()
+
+        recordingThread = object : Thread() {
+            override fun run() {
+                while (isRecording) {
+                    audioRecord!!.read(buffer, 0, bufferSize)
+                    performFFT()
+                }
+            }
+        }
+        recordingThread!!.start()
+        return
+    }
+
+    fun endRec(){
+        if (audioRecord != null && isRecording) {
+            audioRecord!!.stop();
+            audioRecord!!.release();
+            isRecording = false
+        }
+    }
+    private var soundFFT: DoubleArray = DoubleArray(1024)
+    private var soundFFTTemp: DoubleArray = DoubleArray(1024)
+    private var mxIntensity: Double = 0.toDouble()
+    private var soundSegmented: IntArray = intArrayOf(1024)   // dummy initialization
+    private var soundBuffer: ShortArray? = null
+    private var soundBackgroundImage: Bitmap? = null
+
+    fun performFFT() {
+        soundBuffer = ShortArray(1024)
+        soundBackgroundImage = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+        soundSegmented = IntArray(FFT_Len)
+        soundFFT = DoubleArray(FFT_Len * 2)
+        soundFFTMag = DoubleArray(FFT_Len)
+        soundFFTTemp = DoubleArray(FFT_Len * 2)
+
+        for (i in 0..FFT_Len - 1) {
+            soundFFT[i * 2] = buffer[i].toDouble()
+            soundFFT[i * 2 + 1] = 0.0
+        }
+
+        val fft = DoubleFFT_1D(FFT_Len)
+        var index = 0
+        fft.complexForward(soundFFT)
+
+        mx = -99999.0
+        for (i in 0 until FFT_Len) {
+            val re = soundFFT[2 * i]
+            val im = soundFFT[2 * i + 1]
+            soundFFTMag[i] = Math.log(re * re + im * im + 0.001)
+            if (soundFFTMag[i] > mx){
+                mx = soundFFTMag[i]
+                index = i
+            }
+        }
+        //Received start signal, enable message appending
+        if(index == startPw){
+            isStarted = true
+            Log.i("Max Started", "start")
+        }
+        //Recieved end signal, stop appending msg
+        else if(index == endPw){
+            isEnd = true
+            Log.i("Max Ended", "end")
+        }
+        //Received this before allowing each char to append
+        //Used to prevent error in freq recorded due to transitioning to next one
+        if(index == 205){
+            singalRecieved = true
+        }
+
+        if(index > 9 && index < 197 && singalRecieved) {
+
+            if(index == prevIndex){
+                Log.i("Max Repeated Index", "Repeated")
+            }
+
+            else if(isStarted && !isEnd){
+                var ascii_Value = ((index - offset)/2)+32
+                val checkingAscii = ascii_Value
+
+                if(ascii_Value == 123){
+                    ascii_Value = 65
+                }
+                else if(ascii_Value == 124){
+                    ascii_Value = 66
+                }
+                else if(ascii_Value == 125){
+                    ascii_Value = 67
+                }
+
+                val ascii_Char = (ascii_Value).toChar()
+                Log.i("Max index", ascii_Char.toString())
+
+                //indicate that counting is finish
+                if(ascii_Char.equals('.')){
+                    isCounted = true
+                    prevIndex = index
+                    singalRecieved = false
+
+                }
+                //Counting number of occurance by appending the character for the number to currCount and converting that value to string
+                if(isRepeated && !isCounted && !ascii_Char.equals('/')){
+                    if(checkingAscii>=48 && checkingAscii<=57){
+                        currCount = currCount + ascii_Char
+                        count = currCount.toInt()
+                        Log.i("Count char", ascii_Char.toString())
+                        Log.i("Count index", count.toString())
+                        prevIndex = index
+                        singalRecieved = false
+                    }
+                }
+                //Indicate start of counting repeated char
+                if(ascii_Char.equals('/')){
+                    isRepeated = true
+                    var value = ((prevIndex - offset)/2)+32
+                    if(value == 123){
+                        value = 65
+                    }
+                    else if(value == 124){
+                        value = 66
+                    }
+                    else if(value == 125){
+                        value = 67
+                    }
+                    repeatedWord = value.toChar()
+                    Log.i("Repeating started", ascii_Char.toString())
+                    prevIndex = index
+                    singalRecieved = false
+                }
+                //After counting finish number of occurance, append the letter/number to the msg according to count
+                if(isRepeated && isCounted && !ascii_Char.equals('.')){
+                    isRepeated = false
+                    isCounted = false
+                    //Log.i("Count Check", count.toString())
+
+                    for (i in 0 .. count-1){
+                        if(!((checkingAscii>=33&&checkingAscii<=45)||(checkingAscii>=58&&checkingAscii<=67)||(checkingAscii>=91&&checkingAscii<=96))) {
+                            msg = msg + repeatedWord
+                            Log.i("Count msg repeat", msg)
+                            Log.i("Count repeat", repeatedWord.toString())
+                        }
+                    }
+                    count = 0
+                    currCount = ""
+                    prevIndex = index
+                    singalRecieved = false
+                }
+                //For single letters that are non-repeated
+                else if(!isRepeated && !isCounted){
+                    val str = (((index - 10)/2)+32).toString()
+                    if(str != (((prevIndex - 10)/2)+32).toString()){
+                        //exclude these non alpha numeric characters
+                        if(!((checkingAscii>=33&&checkingAscii<=45)||(checkingAscii>=58&&checkingAscii<=67)||(checkingAscii>=91&&checkingAscii<=96))) {
+                            msg = msg + ascii_Char
+                            Log.i("Max char", ascii_Char.toString())
+                            singalRecieved = false
+                        }
+                    }
+                    Log.i("Max index", str)
+                    Log.i("Max prev index", (((prevIndex - 10)/2)+32).toString())
+                    prevIndex = index
+                    count = 0
+                    currCount = ""
+                }
+            }
+        }
+        mxIntensity = mx
+    }
+}
